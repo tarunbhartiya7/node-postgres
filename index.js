6f2ebf1393dc3275e7d00c78025d47b19d9e5eeb
Tarun Bhartiya <tarunbhartiya7@gmail.com>
2022-08-02 16:39:56 +0530

    CRUD APIs

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 require("dotenv").config();
-const { Sequelize, QueryTypes } = require("sequelize");
+const { Sequelize, Model, DataTypes } = require("sequelize");
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3001;
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialectOptions: {
@@ -13,13 +12,71 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   },
 });
 
+class Note extends Model {}
+Note.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    important: {
+      type: DataTypes.BOOLEAN,
+    },
+    date: {
+      type: DataTypes.DATE,
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: "note",
+  }
+);
+Note.sync(); // will create table if not already exists
+
 app.get("/api/notes", async (req, res) => {
-  const notes = await sequelize.query("SELECT * FROM notes", {
-    type: QueryTypes.SELECT,
-  });
+  const notes = await Note.findAll();
+  console.log(JSON.stringify(notes, null, 2));
   res.json(notes);
 });
 
+app.post("/api/notes", async (req, res) => {
+  try {
+    const note = await Note.create(req.body);
+    return res.json(note);
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
+});
+
+app.get("/api/notes/:id", async (req, res) => {
+  const note = await Note.findByPk(req.params.id);
+  if (note) {
+    console.log(note.toJSON());
+    res.json(note);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.put("/api/notes/:id", async (req, res) => {
+  const note = await Note.findByPk(req.params.id);
+  if (note) {
+    note.important = req.body.important;
+    await note.save();
+    res.json(note);
+  } else {
+    res.status(404).end();
+  }
+});
+
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
