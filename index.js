800f68b0f552a62d07ca4dceb3765b4d12f65d26
Tarun Bhartiya <tarunbhartiya7@gmail.com>
2022-08-02 15:23:25 +0530

    first api

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 require("dotenv").config();
 const { Sequelize, QueryTypes } = require("sequelize");
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialectOptions: {
@@ -10,17 +13,13 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   },
 });
 
-const main = async () => {
-  try {
-    await sequelize.authenticate();
-    const notes = await sequelize.query("SELECT * FROM notes", {
-      type: QueryTypes.SELECT,
-    });
-    console.log(notes);
-    sequelize.close();
-  } catch (error) {
-    console.error("Unable to connect to the database:", error);
-  }
-};
+app.get("/api/notes", async (req, res) => {
+  const notes = await sequelize.query("SELECT * FROM notes", {
+    type: QueryTypes.SELECT,
+  });
+  res.json(notes);
+});
 
-main();
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
